37af52d138dbac0588454a0b081dcd671569f86d
Nazlı KARAMAN <karamannazli1997@hotmail.com>
2018-08-30 14:18:46 +0300

    commit1
    with MetromodernUi 1.4.0.0

---
+++ schemaProject/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace schemaProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEsitle = new System.Windows.Forms.Button();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroPanel3 = new MetroFramework.Controls.MetroPanel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.mtxtHedefDatabaseName = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.mtxtHedefServerName = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel2 = new MetroFramework.Controls.MetroPanel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.mtxtDatabaseName = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.mtxtServerName = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.metroPanel1.SuspendLayout();
+            this.metroPanel3.SuspendLayout();
+            this.metroPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEsitle
+            // 
+            this.btnEsitle.Location = new System.Drawing.Point(141, 238);
+            this.btnEsitle.Name = "btnEsitle";
+            this.btnEsitle.Size = new System.Drawing.Size(100, 36);
+            this.btnEsitle.TabIndex = 0;
+            this.btnEsitle.Text = "Eşitle";
+            this.btnEsitle.UseVisualStyleBackColor = true;
+            this.btnEsitle.Click += new System.EventHandler(this.btnEsitle_Click);
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.metroPanel3);
+            this.metroPanel1.Controls.Add(this.metroPanel2);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(16, 18);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(512, 214);
+            this.metroPanel1.TabIndex = 1;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroPanel3
+            // 
+            this.metroPanel3.Controls.Add(this.metroLabel4);
+            this.metroPanel3.Controls.Add(this.mtxtHedefDatabaseName);
+            this.metroPanel3.Controls.Add(this.metroLabel5);
+            this.metroPanel3.Controls.Add(this.mtxtHedefServerName);
+            this.metroPanel3.Controls.Add(this.metroLabel6);
+            this.metroPanel3.HorizontalScrollbarBarColor = true;
+            this.metroPanel3.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel3.HorizontalScrollbarSize = 10;
+            this.metroPanel3.Location = new System.Drawing.Point(247, 15);
+            this.metroPanel3.Name = "metroPanel3";
+            this.metroPanel3.Size = new System.Drawing.Size(206, 196);
+            this.metroPanel3.TabIndex = 10;
+            this.metroPanel3.VerticalScrollbarBarColor = true;
+            this.metroPanel3.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel3.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(62, 6);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(44, 19);
+            this.metroLabel4.TabIndex = 9;
+            this.metroLabel4.Text = "Hedef";
+            // 
+            // mtxtHedefDatabaseName
+            // 
+            // 
+            // 
+            // 
+            this.mtxtHedefDatabaseName.CustomButton.Image = null;
+            this.mtxtHedefDatabaseName.CustomButton.Location = new System.Drawing.Point(91, 1);
+            this.mtxtHedefDatabaseName.CustomButton.Name = "";
+            this.mtxtHedefDatabaseName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mtxtHedefDatabaseName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mtxtHedefDatabaseName.CustomButton.TabIndex = 1;
+            this.mtxtHedefDatabaseName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mtxtHedefDatabaseName.CustomButton.UseSelectable = true;
+            this.mtxtHedefDatabaseName.CustomButton.Visible = false;
+            this.mtxtHedefDatabaseName.Lines = new string[0];
+            this.mtxtHedefDatabaseName.Location = new System.Drawing.Point(76, 59);
+            this.mtxtHedefDatabaseName.MaxLength = 32767;
+            this.mtxtHedefDatabaseName.Name = "mtxtHedefDatabaseName";
+            this.mtxtHedefDatabaseName.PasswordChar = '\0';
+            this.mtxtHedefDatabaseName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mtxtHedefDatabaseName.SelectedText = "";
+            this.mtxtHedefDatabaseName.SelectionLength = 0;
+            this.mtxtHedefDatabaseName.SelectionStart = 0;
+            this.mtxtHedefDatabaseName.ShortcutsEnabled = true;
+            this.mtxtHedefDatabaseName.Size = new System.Drawing.Size(113, 23);
+            this.mtxtHedefDatabaseName.TabIndex = 8;
+            this.mtxtHedefDatabaseName.UseSelectable = true;
+            this.mtxtHedefDatabaseName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mtxtHedefDatabaseName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(4, 63);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(66, 19);
+            this.metroLabel5.TabIndex = 7;
+            this.metroLabel5.Text = "Database:";
+            // 
+            // mtxtHedefServerName
+            // 
+            // 
+            // 
+            // 
+            this.mtxtHedefServerName.CustomButton.Image = null;
+            this.mtxtHedefServerName.CustomButton.Location = new System.Drawing.Point(91, 1);
+            this.mtxtHedefServerName.CustomButton.Name = "";
+            this.mtxtHedefServerName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mtxtHedefServerName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mtxtHedefServerName.CustomButton.TabIndex = 1;
+            this.mtxtHedefServerName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mtxtHedefServerName.CustomButton.UseSelectable = true;
+            this.mtxtHedefServerName.CustomButton.Visible = false;
+            this.mtxtHedefServerName.Lines = new string[0];
+            this.mtxtHedefServerName.Location = new System.Drawing.Point(76, 30);
+            this.mtxtHedefServerName.MaxLength = 32767;
+            this.mtxtHedefServerName.Name = "mtxtHedefServerName";
+            this.mtxtHedefServerName.PasswordChar = '\0';
+            this.mtxtHedefServerName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mtxtHedefServerName.SelectedText = "";
+            this.mtxtHedefServerName.SelectionLength = 0;
+            this.mtxtHedefServerName.SelectionStart = 0;
+            this.mtxtHedefServerName.ShortcutsEnabled = true;
+            this.mtxtHedefServerName.Size = new System.Drawing.Size(113, 23);
+            this.mtxtHedefServerName.TabIndex = 6;
+            this.mtxtHedefServerName.UseSelectable = true;
+            this.mtxtHedefServerName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mtxtHedefServerName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(4, 34);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(51, 19);
+            this.metroLabel6.TabIndex = 5;
+            this.metroLabel6.Text = "Server:";
+            // 
+            // metroPanel2
+            // 
+            this.metroPanel2.Controls.Add(this.metroLabel3);
+            this.metroPanel2.Controls.Add(this.mtxtDatabaseName);
+            this.metroPanel2.Controls.Add(this.metroLabel2);
+            this.metroPanel2.Controls.Add(this.mtxtServerName);
+            this.metroPanel2.Controls.Add(this.metroLabel1);
+            this.metroPanel2.HorizontalScrollbarBarColor = true;
+            this.metroPanel2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.HorizontalScrollbarSize = 10;
+            this.metroPanel2.Location = new System.Drawing.Point(19, 15);
+            this.metroPanel2.Name = "metroPanel2";
+            this.metroPanel2.Size = new System.Drawing.Size(206, 196);
+            this.metroPanel2.TabIndex = 2;
+            this.metroPanel2.VerticalScrollbarBarColor = true;
+            this.metroPanel2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(62, 6);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(49, 19);
+            this.metroLabel3.TabIndex = 9;
+            this.metroLabel3.Text = "Kaynak";
+            // 
+            // mtxtDatabaseName
+            // 
+            // 
+            // 
+            // 
+            this.mtxtDatabaseName.CustomButton.Image = null;
+            this.mtxtDatabaseName.CustomButton.Location = new System.Drawing.Point(91, 1);
+            this.mtxtDatabaseName.CustomButton.Name = "";
+            this.mtxtDatabaseName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mtxtDatabaseName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mtxtDatabaseName.CustomButton.TabIndex = 1;
+            this.mtxtDatabaseName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mtxtDatabaseName.CustomButton.UseSelectable = true;
+            this.mtxtDatabaseName.CustomButton.Visible = false;
+            this.mtxtDatabaseName.Lines = new string[0];
+            this.mtxtDatabaseName.Location = new System.Drawing.Point(76, 59);
+            this.mtxtDatabaseName.MaxLength = 32767;
+            this.mtxtDatabaseName.Name = "mtxtDatabaseName";
+            this.mtxtDatabaseName.PasswordChar = '\0';
+            this.mtxtDatabaseName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mtxtDatabaseName.SelectedText = "";
+            this.mtxtDatabaseName.SelectionLength = 0;
+            this.mtxtDatabaseName.SelectionStart = 0;
+            this.mtxtDatabaseName.ShortcutsEnabled = true;
+            this.mtxtDatabaseName.Size = new System.Drawing.Size(113, 23);
+            this.mtxtDatabaseName.TabIndex = 8;
+            this.mtxtDatabaseName.UseSelectable = true;
+            this.mtxtDatabaseName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mtxtDatabaseName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(4, 63);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(66, 19);
+            this.metroLabel2.TabIndex = 7;
+            this.metroLabel2.Text = "Database:";
+            // 
+            // mtxtServerName
+            // 
+            // 
+            // 
+            // 
+            this.mtxtServerName.CustomButton.Image = null;
+            this.mtxtServerName.CustomButton.Location = new System.Drawing.Point(91, 1);
+            this.mtxtServerName.CustomButton.Name = "";
+            this.mtxtServerName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mtxtServerName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mtxtServerName.CustomButton.TabIndex = 1;
+            this.mtxtServerName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mtxtServerName.CustomButton.UseSelectable = true;
+            this.mtxtServerName.CustomButton.Visible = false;
+            this.mtxtServerName.Lines = new string[0];
+            this.mtxtServerName.Location = new System.Drawing.Point(76, 30);
+            this.mtxtServerName.MaxLength = 32767;
+            this.mtxtServerName.Name = "mtxtServerName";
+            this.mtxtServerName.PasswordChar = '\0';
+            this.mtxtServerName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mtxtServerName.SelectedText = "";
+            this.mtxtServerName.SelectionLength = 0;
+            this.mtxtServerName.SelectionStart = 0;
+            this.mtxtServerName.ShortcutsEnabled = true;
+            this.mtxtServerName.Size = new System.Drawing.Size(113, 23);
+            this.mtxtServerName.TabIndex = 6;
+            this.mtxtServerName.UseSelectable = true;
+            this.mtxtServerName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mtxtServerName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(4, 34);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(51, 19);
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "Server:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Decimal(18,8)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 297);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.btnEsitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel3.ResumeLayout(false);
+            this.metroPanel3.PerformLayout();
+            this.metroPanel2.ResumeLayout(false);
+            this.metroPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEsitle;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroPanel metroPanel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroTextBox mtxtHedefDatabaseName;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroTextBox mtxtHedefServerName;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroPanel metroPanel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroTextBox mtxtDatabaseName;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroTextBox mtxtServerName;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ schemaProject/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace schemaProject
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEsitle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection HBaglanti = new SqlConnection();
+                HBaglanti.ConnectionString = "Server=" + mtxtHedefServerName.Text +
+                                            ";Database=" + mtxtHedefDatabaseName.Text +
+                                            ";Trusted_Connection=true";
+                SqlConnection KBaglanti = new SqlConnection();
+                KBaglanti.ConnectionString = "Server=" + mtxtServerName.Text +
+                                             ";Database=" + mtxtDatabaseName.Text +
+                                             ";Trusted_Connection=true";
+
+                using (KBaglanti)
+                {
+                    KBaglanti.Open();
+                    //get db1 ınformation schema
+                    DataTable InfoSchema = getInfoSchema(KBaglanti);
+                    HBaglanti.Open();
+                    EsitlemeIsl(InfoSchema, HBaglanti);
+                }
+                KBaglanti.Close();
+                HBaglanti.Close();
+                MetroFramework.MetroMessageBox.Show(this, "Esitleme Tamamlandi!");
+            }
+            catch (Exception)
+            {
+                MetroFramework.MetroMessageBox.Show(this, "Bir sorun olustu!");
+            }
+        }
+        public void EsitlemeIsl(DataTable InfoSchema,SqlConnection HBaglanti)
+        {
+            foreach (DataRow row in InfoSchema.Rows)//tek tek tablo bilgilerini çeker
+            {
+                string tablename = row["TABLE_NAME"].ToString();
+                string columnName = row["COLUMN_NAME"].ToString();
+                string columnDefault = row["COLUMN_DEFAULT"].ToString();
+                string isNull = row["IS_NULLABLE"].ToString();
+                string dataType = row["DATA_TYPE"].ToString();
+
+                SqlCommand setNull;
+                SqlCommand defValue;
+
+                if (isNull == "YES")
+                {
+                    setNull = new SqlCommand("ALTER TABLE @tablename alter column @columnName @dataType null", HBaglanti);
+                    setNull.Parameters["@tablename"].Value = tablename;
+                    setNull.Parameters["@columnName"].Value = columnName;
+                    setNull.Parameters["@dataType"].Value = dataType;
+                    setNull.ExecuteScalar();
+                }
+                else if (isNull == "NO")
+                {
+                    setNull = new SqlCommand("ALTER TABLE @tablename alter column @columnName @dataType not null", HBaglanti);
+                    setNull.Parameters["@tablename"].Value = tablename;
+                    setNull.Parameters["@columnName"].Value = columnName;
+                    setNull.Parameters["@dataType"].Value = dataType;
+                    setNull.ExecuteScalar();
+                }
+                if (columnDefault == "((0))")
+                {
+                    try
+                    {
+                        defValue = new SqlCommand("ALTER TABLE @tablename ADD CONSTRAINT @RandomString DEFAULT (0) FOR @columnName;", HBaglanti);
+                        defValue.Parameters["@tablename"].Value = tablename;
+                        defValue.Parameters["@RandomString"].Value = RandomString();
+                        defValue.Parameters["@columnName"].Value = columnName;
+                        defValue.ExecuteNonQuery();
+                    }
+                    catch (System.Data.SqlClient.SqlException)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+        public void dataTyIslem(SqlConnection connection)
+        {
+            SqlCommand command=new SqlCommand("BEGIN DECLARE @DataType VARCHAR(20), @ColumnName varchar(50), @TableName VARCHAR(50), @sql nvarchar(max)"+
+            " DECLARE C1 CURSOR FOR select DATA_TYPE, COLUMN_NAME, TABLE_NAME from INFORMATION_SCHEMA.COLUMNS "+
+                    "where TABLE_NAME IN "+
+                    "(Select TABLE_NAME from @dbName.INFORMATION_SCHEMA.TABLES ) "+
+                    "and DATA_TYPE = 'decimal'"+
+            "OPEN C1; "+
+            "FETCH NEXT FROM C1 "+
+            "INTO @DataType, @ColumnName, @TableName; " +
+            "WHILE @@FETCH_STATUS = 0 " +
+            "BEGIN " +
+            "set @sql = 'alter table ' + @TableName + ' alter column ' + @ColumnName + ' decimal(18,8)' " +
+            "exec sp_executesql @sql " +
+            "FETCH NEXT FROM C1 " +
+            "INTO @DataType, @ColumnName, @TableName; " +
+            "END " +
+            "Close C1 "+
+            "END",connection); //sadece veritabanı ismini alarak tüm decimal degerleri decimal(18,8) yapan komut
+            command.Parameters["@dbName"].Value = getDbNme(connection);
+            command.ExecuteNonQuery();
+        }
+        public string getDbNme(SqlConnection connection)
+        {
+           string conString= connection.ConnectionString;
+            string[] words = conString.Split(';');
+
+            return words[1].Substring(9);
+
+        }
+        public DataTable getInfoSchema(SqlConnection baglanti)
+        {
+            SqlCommand getInfoSchema = new SqlCommand("select * from INFORMATION_SCHEMA.COLUMNS ", baglanti);
+            SqlDataReader reader = getInfoSchema.ExecuteReader();
+            DataTable InfoSchema = new DataTable();
+            InfoSchema.Load(reader);
+            return InfoSchema;
+        }
+        private static Random random = new Random();
+        public static string RandomString()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return "DF_"+new string(Enumerable.Repeat(chars,5)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection HBaglanti = new SqlConnection();
+                HBaglanti.ConnectionString = "Server=" + mtxtHedefServerName.Text +
+                                            ";Database=" + mtxtHedefDatabaseName.Text +
+                                            ";Trusted_Connection=true";
+                dataTyIslem(HBaglanti);
+                HBaglanti.Close();
+                MetroFramework.MetroMessageBox.Show(this,"veritabanindaki tum decimal degerler decimal(18,8) ile degistirildi.");
+            }
+            catch (Exception)
+            {
+
+                MetroFramework.MetroMessageBox.Show(this,"Bir sorun olustu.");
+            }
+        }
+    }
+}
